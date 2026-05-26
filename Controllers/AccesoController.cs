e1b0704e183afbca0b0f346b97f5b66e8e454002
Agustin671 <agustincanas1@gmail.com>
2026-05-26 08:32:40 -0400

    Codigo

--- Controllers/AccesoController.cs
+++ Controllers/AccesoController.cs
@@ -1,23 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http; // Librería para usar sesiones
+using Microsoft.AspNetCore.Http;
 
 namespace Farmacia.Controllers
 {
     public class AccesoController : Controller
     {
-        // GET: Muestra el Login
+
         public IActionResult Login()
         {
             return View();
         }
 
-        // POST: Revisa la clave
         [HttpPost]
         public IActionResult Login(string usuario, string clave)
         {
             if (usuario == "admin" && clave == "12345")
             {
-                // Guardamos una Variable de Sesión. Esta no se borra al cambiar de página.
                 HttpContext.Session.SetString("UsuarioAdmin", "Activo");
 
                 return RedirectToAction("Admin", "Medicamentos");
