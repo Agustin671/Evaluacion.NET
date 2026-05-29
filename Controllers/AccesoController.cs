78f3ff453846a29f377c7220c3f5df0274d85bbc
Agustin671 <agustincanas1@gmail.com>
2026-05-28 21:25:56 -0400

    Update AccesoController.cs

--- Controllers/AccesoController.cs
+++ Controllers/AccesoController.cs
@@ -14,13 +14,14 @@ namespace Farmacia.Controllers
         [HttpPost]
         public IActionResult Login(string usuario, string clave)
         {
+            // Validación de credenciales de acceso
             if (usuario == "admin" && clave == "12345")
             {
                 HttpContext.Session.SetString("UsuarioAdmin", "Activo");
-
+           // Redirección HTTP hacia el controlador de Medicamentos, acción Admin.
                 return RedirectToAction("Admin", "Medicamentos");
             }
-
+           // Si falla la autenticación muestra el mensaje de error a la capa de presentación.
             ViewBag.Error = "Datos incorrectos, intenta de nuevo.";
             return View();
         }
